085c6ec8a2442e330afeeea6a952236bc7a5c6a8
Nikita Dolbilov <nikita1100@bk.ru>
2023-04-21 12:25:12 +0400

    readme.md

--- src/main/java/com/example/accesskeybackend/template/controller/WebController.java
+++ src/main/java/com/example/accesskeybackend/template/controller/WebController.java
@@ -2,13 +2,11 @@ package com.example.accesskeybackend.template.controller;
 
 import com.example.accesskeybackend.template.service.WebService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/web")
